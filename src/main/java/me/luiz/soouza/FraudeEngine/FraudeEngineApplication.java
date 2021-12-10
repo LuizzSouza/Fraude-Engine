ad86217cdae11baef17a5983241130b71c09db3e
Luiz Souza <luixsouza12@outlook.com>
2021-12-10 18:54:50 -0300

    Initial commit with postgress connection

---
+++ src/main/java/me/luiz/soouza/FraudeEngine/FraudeEngineApplication.java
@@ -0,0 +1,13 @@
+package me.luiz.soouza.FraudeEngine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FraudeEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FraudeEngineApplication.class, args);
+	}
+
+}
